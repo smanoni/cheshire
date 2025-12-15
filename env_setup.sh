2b6627f88e98e7fc67c781446bcee9f231c62290
Simone Manoni <s.manoni@unibo.it>
2025-12-15 15:28:41 +0100

    update envsetup

--- env_setup.sh
+++ env_setup.sh
@@ -1,6 +1,5 @@
-bash
+source .bashrc
 export PATH=/usr/local/bin:$PATH
 export MGC_HOME=/sw/CAD/Mentor/questa
 export PATH=/sw/CAD/Mentor/questa/2022.4/questasim/bin:$PATH
 export PATH=/sw/riscV64/riscv64-gcc-13.2.0/bin:$PATH
-source .bashrc
